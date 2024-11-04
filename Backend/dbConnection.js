e5bdb49174d00834ae780d40fb74a3d0d40f92ed
rkddbwns0 <112998995+rkddbwns0@users.noreply.github.com>
2024-11-04 13:59:38 +0900

    Update dbConnection.js

--- Backend/dbConnection.js
+++ Backend/dbConnection.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'plant',
-    password: 'plt1234',
-    database: 'plantdb',
+    user: '',
+    password: '',
+    database: '',
     dateStrings: 'date',
 });
 
